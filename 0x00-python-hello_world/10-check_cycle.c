e4e29cd730bdd9059e59076878713d51a4dcc428
badarbenami <badarbenami1@gmail.com>
2023-11-27 16:32:33 -0800

    0x00. Python - Hello, World

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a linked list contains a cycle
+ * check_cycle --- checks if a linked list contains a cycle
  *
- * Return: 1 if the list has a cycle, 0 if it doesn't
+ * Return: 1 if the list has a cycle, zero if it doesn't.
  */
 int check_cycle(listint_t *list)
 {
