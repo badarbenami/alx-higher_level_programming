e850b474564149b12731df97720f45bd64c9aaed
badarbenami <badarbenami1@gmail.com>
2023-11-28 15:32:04 -0800

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -4,12 +4,12 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - single-link list.
+ * struct listint_s --- singly linked list.
  * @n: Integer.
- * @next: indicates the following node.
+ * @next: points to the next node.
  *
- * Description: Structure of single linked list nodes
- * for the Holberton project.
+ * Description: singly linked list node structure.
+ * for ALX project.
  */
 typedef struct listint_s
 {
@@ -20,6 +20,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+
 listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
